c9be54672bf52c2ccb1c64566e42407b90e9fd80
Seyed Jalal Hosseini <jalalhosseiny@gmail.com>
2015-12-30 16:10:36 +0100

    Initial Commit

---
+++ src/main/java/com/artronics/gsdwn/test/GsdwnTestApplication.java
@@ -0,0 +1,12 @@
+package com.artronics.gsdwn.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GsdwnTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GsdwnTestApplication.class, args);
+	}
+}
